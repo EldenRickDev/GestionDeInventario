af5edf61168b954c266d7578b018ab01ecd024cc
EldenRickDev <eldenrickdev@gmail.com>
2024-08-21 05:49:47 -0400

    Initial Commit

---
+++ GestionDeInventario/GestionDeInventarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestionDeInventarioApp.swift
+//  GestionDeInventario
+//
+//  Created by Ricardo Andrés Gatica Collarte on 21-08-24.
+//
+
+import SwiftUI
+
+@main
+struct GestionDeInventarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
